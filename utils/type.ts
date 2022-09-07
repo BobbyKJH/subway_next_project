14a89593aa62ea11f2aff5530d6ea4a10f3c5a3f
BobbyKJH <rlatnguscjsw@gmail.com>
2022-09-08 00:07:56 +0900

    Reafactor: Header Link href && ResetStyle white-space -> pre-line

---
+++ utils/type.ts
@@ -0,0 +1,30 @@
+// Card Type
+export interface MapType {
+  id: number;
+  name: string;
+  eng_name: string;
+  img: string;
+  map?: any;
+}
+
+// 상세 페이지 Type
+export interface ProductType {
+  calorie: number;
+  img: string;
+  name: string;
+  eng_name: string;
+  summary: string;
+}
+
+export interface MenuType {
+  img: string;
+  name: string;
+  eng: string;
+  summary?: string;
+  calorie: number;
+}
+
+// 모두 string
+export interface StringType {
+  [key: string]: string;
+}
