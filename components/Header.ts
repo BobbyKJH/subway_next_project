ce19a468b3b78db55aa2a8e4d4b0e9cb4536cef2
BobbyKJH <rlatnguscjsw@gmail.com>
2022-09-11 00:09:41 +0900

    Feat: Header 스타일 경로 변경

--- components/Header.ts
+++ components/Header.ts
@@ -3,14 +3,13 @@ import styled from "styled-components";
 export const HeaderStyle = styled.div`
   display: block;
   background-color: #fff;
-  box-shadow: 2px 2px 2px 0 #000;
   box-sizing: border-box;
+  padding: 0 1rem;
   #header {
     display: flex;
     justify-content: space-between;
     margin: 0 auto;
     padding: 1rem 0;
-    width: 100%;
     max-width: 1200px;
 
     #title {
@@ -20,11 +19,12 @@ export const HeaderStyle = styled.div`
       color: #000;
       padding: 0 1rem;
       font-weight: 900;
-      font-size: 1.5rem;
+      font-size: 1.3rem;
     }
 
     .header {
       display: flex;
+      background-color: aqua;
       hr {
         display: block;
         margin: auto 0;
@@ -36,13 +36,24 @@ export const HeaderStyle = styled.div`
         justify-content: center;
         align-items: center;
         box-sizing: border-box;
-        font-size: 0.8em;
+        font-size: 0.5em;
         padding: 0 1rem;
         width: 140px;
         color: #80808090;
         &:hover {
           color: #000;
         }
+      }
+    }
+  }
+
+  @media (max-width: 1000px) {
+    display: block;
+    #header {
+      display: block;
+      padding: 0;
+      #title {
+        padding: 1rem 0;
       }
     }
   }
