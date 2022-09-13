977b44ef8f6ebbeb128b8b73f5d786d97b5e5650
BobbyKJH <rlatnguscjsw@gmail.com>
2022-09-14 00:09:01 +0900

    Style: MenuList Page 스타일 생성

---
+++ styles/pages/MenuPage.styled.ts
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+export const MenuPage = styled.div``;
+
+export const MenuCard = styled.div`
+  margin: 0 auto;
+  width: 66rem;
+  @media (max-width: 67rem) {
+    width: 100%;
+  }
+`;
