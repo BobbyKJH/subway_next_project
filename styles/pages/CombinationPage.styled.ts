4dba6e42691325c195f4910ab95e267d16bc4c2b
BobbyKJH <rlatnguscjsw@gmail.com>
2022-09-18 00:03:55 +0900

    Style: Combination Page Style

---
+++ styles/pages/CombinationPage.styled.ts
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+export const CombinationPage = styled.div`
+  display: block;
+  margin: 0 auto;
+  width: 72rem;
+  @media (max-width: 72rem) {
+    width: 54rem;
+  }
+  @media (max-width: 54rem) {
+    width: 36rem;
+  }
+`;
