6306913c542f00ed7e9d1af77612cc82dd29a93f
BobbyKJH <rlatnguscjsw@gmail.com>
2022-09-20 00:02:14 +0900

    Style: Combination Page Style 적용

--- pages/combination/sandwich/index.tsx
+++ pages/combination/sandwich/index.tsx
@@ -4,12 +4,19 @@ import React from "react";
 import { useRouter } from "next/router";
 // Redux
 import { useAppDispatch } from "../../../store/hooks";
-import { selectName, selectSandwich } from "../../../store/recipeSlice";
+import {
+  selectEng,
+  selectName,
+  selectSandwich,
+} from "../../../store/recipeSlice";
 // Component
 import MenuComination from "../../../components/combination/MenuCombination";
 import MenuList from "../../../components/combination/MenuList";
 // Style
-import { CombinationPage } from "../../../styles/pages/CombinationPage.styled";
+import {
+  CombinationPage,
+  CombinationTitle,
+} from "../../../styles/pages/CombinationPage.styled";
 import { CombinationButton } from "../../../styles/components/combination/MenuCombination.styled";
 // Type
 import { MapType, ProductType } from "../../../utils/type";
@@ -20,19 +27,25 @@ const Sandwich = ({ sandwich }: { sandwich: MapType }) => {
 
   // 샌드위치 이름 및 이미지 선택
   const SandwichMenu = (e: React.MouseEvent<HTMLButtonElement>) => {
-    const { value, name } = e.currentTarget;
+    const { value, name, id } = e.currentTarget;
     dispatch(selectName(name));
     dispatch(selectSandwich(value));
+    dispatch(selectEng(id));
     router.push("/combination/bread");
   };
 
   return (
     <CombinationPage>
+      <CombinationTitle>
+        <span>샌드위치</span>
+      </CombinationTitle>
+
       {sandwich.map((item: ProductType) => (
         <CombinationButton
           onClick={SandwichMenu}
           value={item.img}
           name={item.name}
+          id={item.eng_name}
           key={item.id}
         >
           {/* 카드 */}
--- pages/combination/sauce/index.tsx
+++ pages/combination/sauce/index.tsx
@@ -2,11 +2,13 @@
 import axios from "axios";
 import React, { useState } from "react";
 // Component
-import RecipeBtn from "../../../components/combination/RecipeBtn";
 import MenuComination from "../../../components/combination/MenuCombination";
 import MenuList from "../../../components/combination/MenuList";
 // Style
-import { CombinationPage } from "../../../styles/pages/CombinationPage.styled";
+import {
+  CombinationPage,
+  CombinationTitle,
+} from "../../../styles/pages/CombinationPage.styled";
 import { CombinationButton } from "../../../styles/components/combination/MenuCombination.styled";
 // Type
 import { MapType, ProductType } from "../../../utils/type";
@@ -21,6 +23,11 @@ const Sauce = ({ sauce }: { sauce: MapType }) => {
 
   return (
     <CombinationPage>
+      <CombinationTitle>
+        <span>소스</span>
+        <p className="tip">소스는 최대 3개 선택 가능 합니다.</p>
+      </CombinationTitle>
+
       {sauce.map((item: ProductType) => (
         <CombinationButton onClick={SauceMenu} value={item.name} key={item.id}>
           {/* 카드 */}
@@ -34,9 +41,8 @@ const Sauce = ({ sauce }: { sauce: MapType }) => {
       ))}
 
       {/* 메뉴 선택 리스트 */}
-      <MenuList sauce={arr.slice(0, 3)} />
+      <MenuList sauce={arr} />
       {/* 메뉴 리스트 완성 버튼 */}
-      <RecipeBtn arr={arr} />
     </CombinationPage>
   );
 };
