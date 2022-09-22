073d4afe05a82f8cd82a09ce94235918cf27744f
BobbyKJH <rlatnguscjsw@gmail.com>
2022-09-23 00:02:02 +0900

    Feat: MobileHeader, Header innerHtml 1000

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,30 +1,72 @@
 // React
 import Link from "next/link";
-import React from "react";
+import { useRouter } from "next/router";
+import React, { useEffect, useState } from "react";
+// Component
+import MobileHeader from "./MobileHeader";
 // Style
+import { HeaderNav, HeaderStyle, HeaderTitle } from "../styles/Header.styled";
 
 const Header = () => {
+  const router = useRouter();
+
+  const Page = (title: string) => {
+    return router.pathname.includes(title);
+  };
+
+  const [mode, setMode] = useState(true);
+
+  const MobileMode = () => {
+    if (window.innerWidth > 1000) {
+      setMode(true);
+    } else {
+      setMode(false);
+    }
+  };
+
+  useEffect(() => {
+    if (window.innerWidth <= 1000) {
+      setMode(false);
+    }
+    window.addEventListener("resize", MobileMode);
+    return () => {
+      window.removeEventListener("resize", MobileMode);
+    };
+  }, []);
+
   return (
     <>
-      <div id="header">
-        <Link href={"/"}>
-          <a id="title">My Subway Recipe</a>
-        </Link>
+      {mode ? (
+        <HeaderStyle>
+          <div className="header">
+            <Link href={"/"}>
+              <HeaderTitle id="title">My Subway</HeaderTitle>
+            </Link>
 
-        <div className="header">
-          <Link href={"/menu/sandwich"}>
-            <a className="nav">메뉴소개</a>
-          </Link>
+            <div>
+              <Link href={"/menu/sandwich"}>
+                <HeaderNav className={Page("/menu") ? "active" : ""}>
+                  메뉴
+                </HeaderNav>
+              </Link>
 
-          <Link href={"/combination/sandwich"}>
-            <a className="nav">조합</a>
-          </Link>
+              <Link href={"/combination/sandwich"}>
+                <HeaderNav className={Page("/combination") ? "active" : ""}>
+                  조합
+                </HeaderNav>
+              </Link>
 
-          <Link href={"/mypage"}>
-            <a className="nav">마이페이지</a>
-          </Link>
-        </div>
-      </div>
+              <Link href={"/mypage"}>
+                <HeaderNav className={Page("/mypage") ? "active" : ""}>
+                  마이 페이지
+                </HeaderNav>
+              </Link>
+            </div>
+          </div>
+        </HeaderStyle>
+      ) : (
+        <MobileHeader />
+      )}
     </>
   );
 };
