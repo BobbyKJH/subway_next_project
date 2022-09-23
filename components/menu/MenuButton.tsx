0b2be67c3526a308b6e26d74f3e052828b5380e6
BobbyKJH <rlatnguscjsw@gmail.com>
2022-09-23 11:49:03 +0900

    Refactor: Router && Style 적용

--- components/menu/MenuButton.tsx
+++ components/menu/MenuButton.tsx
@@ -1,25 +1,37 @@
 // React
 import Link from "next/link";
 import React from "react";
+import { useRouter } from "next/router";
 // Style
 import { MenuButtonBox } from "../../styles/components/menu/MenuButton.styled";
 
 const MenuButton = () => {
+  const router = useRouter();
+
+  const menu = (title: string) => router.pathname === `/menu/${title}`;
+
   return (
-    <>
+    <MenuButtonBox>
       <Link href={`/menu/sandwich`} id="sandwich">
-        <a>샌드위치</a>
+        <a className={menu("sandwich") ? "active" : ""}>샌드위치</a>
       </Link>
+
       <Link href={`/menu/bread`} id="bread">
-        <a>빵</a>
+        <a className={menu("bread") ? "active" : ""}>빵</a>
       </Link>
+
       <Link href={`/menu/cheese`} id="cheese">
-        <a>치즈</a>
+        <a className={menu("cheese") ? "active" : ""}>치즈</a>
       </Link>
+
+      <Link href={`/menu/vegetable`} id="vegetable">
+        <a className={menu("vegetable") ? "active" : ""}>채소</a>
+      </Link>
+
       <Link href={`/menu/sauce`} id="sauce">
-        <a>소스</a>
+        <a className={menu("sauce") ? "active" : ""}>소스</a>
       </Link>
-    </>
+    </MenuButtonBox>
   );
 };
 
