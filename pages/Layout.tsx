e7292d4e35209b19feaad18ee5f0684886f08350
BobbyKJH <rlatnguscjsw@gmail.com>
2022-09-24 12:36:33 +0900

    Refactor: Header && Header Style

--- pages/Layout.tsx
+++ pages/Layout.tsx
@@ -1,5 +1,8 @@
+// React
 import React from "react";
+// Component
 import Header from "../components/Header";
+import Footer from "../components/layout/Footer";
 
 interface Type {
   children: React.ReactNode;
@@ -9,6 +12,7 @@ const Layout = ({ children }: Type) => (
   <>
     <Header />
     {children}
+    <Footer />
   </>
 );
 
