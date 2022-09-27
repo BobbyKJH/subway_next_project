2fdc774eab8d4dc57bdd5ff81e69916363ae7c02
BobbyKJH <rlatnguscjsw@gmail.com>
2022-09-27 13:41:52 +0900

    Style: 조합 페이지 Style 구성
    1) 내용
    
    -  조합 페이지 컴포넌트 Style 수정
    
    2) Git Type
    
      1. Feat: 새로운 기능 개발
    
      2. Fix: 버그 등 수정
    
      3. Docs: 문서 내용 변경
    
      4. Style: 코드가 아닌 스타일에 관련된 수정
    
      5. Refactor: 리팩토링 코드

--- styles/components/combination/MenuCombination.styled.ts
+++ styles/components/combination/MenuCombination.styled.ts
@@ -8,6 +8,7 @@ export const CombinationButton = styled.button`
   background-color: #fff;
   margin: 1rem;
   width: 22rem;
+  padding: 0;
   border: 5px solid #009000;
   border-radius: 10px;
   box-shadow: 0px 7px 0 0px #00000080;
@@ -18,7 +19,7 @@ export const CombinationButton = styled.button`
   }
   @media (max-width: 54rem) {
     width: 90%;
-    display: flex;
+    display: block;
     margin: 0 auto 2.5rem auto;
   }
   &:hover {
@@ -29,47 +30,51 @@ export const CombinationButton = styled.button`
 
 // 조합 이미지
 export const CombinationImg = styled.img`
-  box-sizing: border-box;
   width: 100%;
   @media (max-width: 54rem) {
-    width: 50%;
+    margin: auto;
+    width: 70%;
   }
 `;
 
 // 조합 Name Eng_Name Calorie
 export const CombinationContent = styled.div`
-  display: inline-block;
   position: relative;
   box-sizing: border-box;
-  line-height: 24px;
+  line-height: 30px;
   margin: auto;
   .name {
     display: block;
     font-size: 1rem;
     font-weight: 900;
+    color: #000;
   }
   .eng {
+    display: block;
     font-size: 0.8rem;
     color: #ffa500;
   }
   .calorie {
     display: block;
-    margin: 0.8rem 0 0.8rem 0;
-    font-size: 0.7rem;
+    font-size: 0.8rem;
+    line-height: 10px;
+    padding: 0 0 1rem 0;
   }
   @media (max-width: 54rem) {
-    margin: auto;
     .calorie {
-      margin: 2rem 0 0 0;
+      margin: 0.5rem 0 0 0;
     }
   }
   @media (max-width: 35rem) {
     margin: auto;
     .name {
-      font-size: 0.8rem;
+      font-size: 14px;
     }
     .eng {
-      font-size: 0.3rem;
+      font-size: 10px;
+    }
+    .calorie {
+      font-size: 10px;
     }
   }
 `;
--- styles/components/combination/Recipe.styled.ts
+++ styles/components/combination/Recipe.styled.ts
@@ -3,98 +3,116 @@ import styled from "styled-components";
 export const ResultStyle = styled.div`
   box-sizing: border-box;
   background-color: #fff;
-  margin: 1.5rem auto 7rem auto;
-  width: 30rem;
+  margin: 0 auto;
+  width: 40rem;
+  height: 95vh;
   border-radius: 15px;
   @media (max-width: 54rem) {
+    width: 70%;
+  }
+  @media (max-width: 500px) {
+    width: 100%;
+  }
+`;
+
+// 샌드위치 이미지
+export const ResultImg = styled.img`
+  display: block;
+  margin: 0 auto;
+  width: 25rem;
+  @media (max-width: 500px) {
     width: 90%;
   }
 `;
 
-export const ResultImg = styled.img`
-  display: block;
-  margin: 0 auto;
-  width: 30rem;
-  @media (max-width: 54rem) {
-    width: 25rem;
-  }
-`;
-
+// 샌드위치 이름
 export const ResultSandwich = styled.span`
   display: block;
   text-align: center;
-  font-size: 1.7rem;
+  font-size: 1.5rem;
   font-weight: 900;
+  margin: 0 0 20px 0;
   @media (max-width: 54rem) {
     font-size: 1.2rem;
   }
+  @media (max-width: 500px) {
+    font-size: 1.1rem;
+  }
 `;
 
-export const ResultBread = styled.div`
+// 빵, 치즈 이름 및 이미지
+export const ResultList = styled.div`
   display: inline-block;
   box-sizing: border-box;
   text-align: center;
-  margin: 1rem auto 0 auto;
   width: 50%;
   div {
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-    padding: 2rem 1.7rem;
+    display: block;
   }
   span {
+    display: block;
+    margin: 0 auto;
+    border-radius: 10px;
+    font-size: 1.1rem;
     font-weight: 900;
-    font-size: 1.2rem;
   }
   p {
-    font-size: 1rem;
+    font-size: 0.8rem;
   }
-  .arrow {
-    ::after {
-      content: "➡️";
-    }
+  img {
+    width: 10rem;
   }
-  @media (max-width: 54rem) {
+  @media (max-width: 500px) {
     span {
-      font-size: 1rem;
+      font-size: 0.8rem;
+      width: 80%;
     }
     p {
-      font-size: 0.9rem;
+      font-size: 0.7rem;
+    }
+    img {
+      width: 50%;
+      margin: 0 auto 10px auto;
     }
   }
 `;
 
-export const ResultCheese = styled(ResultBread)``;
-
-export const ResultSauce = styled.div<{ width: number }>`
+// 소스 이미지 및 이름
+export const ResultSauce = styled.div`
   display: block;
   box-sizing: border-box;
+  text-align: center;
   span {
     display: block;
-    text-align: center;
-    padding: 10px 0;
-    font-size: 1.2rem;
+    font-size: 1.1rem;
     font-weight: 900;
   }
-  p {
-    font-size: 1rem;
-    padding: 1rem 0;
-    display: inline-block;
-    box-sizing: border-box;
-    text-align: center;
-    width: ${(props) => 100 / props.width}%;
+  .image {
+    display: block;
+    margin: 0 auto;
+    width: 90%;
   }
-  @media (max-width: 54rem) {
+  .name {
+    display: block;
+  }
+  img {
+    width: 30%;
+  }
+  p {
+    display: inline-block;
+    width: 33%;
+    font-size: 0.5rem;
+  }
+  @media (max-width: 500px) {
     span {
-      font-size: 1rem;
+      font-size: 0.8rem;
     }
     p {
-      font-size: 0.9rem;
+      font-size: 8px;
     }
   }
 `;
 
 export const ResultCalorie = styled.p`
-  margin: 1rem 0 0 0;
   text-align: center;
 `;
