7c73278f875498625c97f2550ff4e0c914d77954
BobbyKJH <rlatnguscjsw@gmail.com>
2022-09-29 13:32:08 +0900

    Fix: MyPage 404 Error
    1) 내용
    
    -  MyRecipe Error Fix
    
    2) Git Type
    
      1. Feat: 새로운 기능 개발
    
      2. Fix: 버그 등 수정
    
      3. Docs: 문서 내용 변경
    
      4. Style: 코드가 아닌 스타일에 관련된 수정
    
      5. Refactor: 리팩토링 코드

--- components/mypage/MyRecipe.tsx
+++ components/mypage/MyRecipe.tsx
@@ -1,4 +1,3 @@
-// React
 import React from "react";
 import Link from "next/link";
 // Component
@@ -6,14 +5,9 @@ import RecipeList from "./RecipeList";
 // Style
 import {
   MakeStyle,
-  MyRecipeKcal,
-  MyRecipeName,
   MyRecipeSandwich,
-  MyRecipeSauce,
   MyRecipeStyle,
 } from "../../styles/components/mypage/MyRecipe.styled";
-// Utils
-import { AddCommas } from "../../utils/utils";
 
 const MyRecipe = () => {
   const menu = JSON.parse(localStorage.getItem("recipe") as string);
@@ -22,31 +16,9 @@ const MyRecipe = () => {
     <>
       {menu ? (
         <MyRecipeStyle>
-          {/* 샌드위치 이미지 */}
           <MyRecipeSandwich src={menu.sandwich} alt={menu.sandwich} />
 
-          <MyRecipeName>{menu.name}</MyRecipeName>
-
-          {/* 빵 이름 및 이미지 */}
           <RecipeList title="빵" img={menu.breadImg} name={menu.bread} />
-
-          {/* 치즈 이름 및 이미지 */}
-          <RecipeList title="치즈" img={menu.cheeseImg} name={menu.cheese} />
-
-          {/* 소스 선택 종류 Style 예정 */}
-          <MyRecipeSauce>
-            <span>소스</span>
-            {menu.sauceImg.map((item: string, idx: number) => (
-              <img src={item} key={idx} />
-            ))}
-
-            {menu.sauce.map((item: string, idx: number) => (
-              <p key={idx}>{item}</p>
-            ))}
-          </MyRecipeSauce>
-
-          {/* Kcal */}
-          <MyRecipeKcal>{AddCommas(menu.kcal)} Kcal</MyRecipeKcal>
         </MyRecipeStyle>
       ) : (
         <MakeStyle>
