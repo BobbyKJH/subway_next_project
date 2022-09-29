c545dd0ce2b77d87f106d734d78a5469b50d01a7
BobbyKJH <rlatnguscjsw@gmail.com>
2022-09-29 13:08:51 +0900

    Fix: MyPage Mobile 404 Error
    1) 내용
    
    -  Mobile MyPage 404 Error
    
    2) Git Type
    
      1. Feat: 새로운 기능 개발
    
      2. Fix: 버그 등 수정
    
      3. Docs: 문서 내용 변경
    
      4. Style: 코드가 아닌 스타일에 관련된 수정
    
      5. Refactor: 리팩토링 코드

--- pages/mypage/index.tsx
+++ pages/mypage/index.tsx
@@ -3,27 +3,11 @@ import React, { useState } from "react";
 // Component
 import FavoriteCollection from "../../components/mypage/FavoriteCollection";
 import MyRecipe from "../../components/mypage/MyRecipe";
-// Style
-import { ListButton } from "../../styles/pages/MyPage.styled";
 
 const MyPage = () => {
-  const [recipe, setRecipe] = useState(true);
-  const [like, setLike] = useState(false);
-
-  const RecipeButton = () => {
-    setRecipe(!recipe);
-  };
-
-  const LikeButton = () => {
-    setLike(!like);
-  };
-
   return (
     <>
-      <ListButton arrow={recipe} onClick={RecipeButton}>
-        나만의 레시피
-      </ListButton>
-      <>{recipe && <MyRecipe />}</>
+      <MyRecipe />
     </>
   );
 };
