dfe941413c51201151d51a2e220749cc37b02348
BobbyKJH <rlatnguscjsw@gmail.com>
2022-10-02 00:23:36 +0900

    Style: RecipeSauce Style
    1) 내용
    
    -  나만의 레시피 소스 component Style
    
    2) Git Type
    
      1. Feat: 새로운 기능 개발
    
      2. Fix: 버그 등 수정
    
      3. Docs: 문서 내용 변경
    
      4. Style: 코드가 아닌 스타일에 관련된 수정
    
      5. Refactor: 리팩토링 코드

---
+++ styles/components/mypage/RecipeSauce.styled.ts
@@ -0,0 +1,22 @@
+import styled from "styled-components";
+// 전체 이미지
+export const RecipeSauceStyle = styled.div`
+  display: block;
+  text-align: center;
+  span {
+    display: block;
+    line-height: 30px;
+    font-weight: 900;
+  }
+`;
+// 소스 이미지
+export const RecipeSauceImage = styled.img`
+  display: inline-block;
+  width: 30%;
+`;
+
+export const RecipeSauceName = styled.p`
+  display: inline-block;
+  font-size: 0.4rem;
+  width: 30%;
+`;
