2aa78889b9e7d600dc9637ba3618f38c17d00d44
BobbyKJH <rlatnguscjsw@gmail.com>
2022-10-13 11:47:01 +0900

    Style: ResetBtn Style
    1) 내용
    
    -  position absolute && Style
    
    2) Git Type
    
      1. Feat: 새로운 기능 개발
    
      2. Fix: 버그 등 수정
    
      3. Docs: 문서 내용 변경
    
      4. Style: 코드가 아닌 스타일에 관련된 수정
    
      5. Refactor: 리팩토링 코드

--- styles/components/combination/RecipeBtn.styled.ts
+++ styles/components/combination/RecipeBtn.styled.ts
@@ -4,9 +4,13 @@ export const RecipeButton = styled.button`
   display: block;
   position: absolute;
   box-sizing: border-box;
+  border: 3px solid #000;
   top: 20px;
   font-weight: 900;
   background-color: #fff;
   color: #000;
-  border: none;
+  &:hover {
+    background-color: #000;
+    color: #fff;
+  }
 `;
