5d91df5dfb65ab8b06f85bcf2f4382c3b64d511e
BobbyKJH <rlatnguscjsw@gmail.com>
2022-10-13 11:57:47 +0900

    Feat: MyPage button
    1) 내용
    
    -  나만의 레시피 및 좋아하는 메뉴 보여주는 버튼 추가
    
    2) Git Type
    
      1. Feat: 새로운 기능 개발
    
      2. Fix: 버그 등 수정
    
      3. Docs: 문서 내용 변경
    
      4. Style: 코드가 아닌 스타일에 관련된 수정
    
      5. Refactor: 리팩토링 코드

--- pages/mypage/index.tsx
+++ pages/mypage/index.tsx
@@ -3,13 +3,51 @@ import React, { useState } from "react";
 // Component
 import FavoriteCollection from "../../components/mypage/FavoriteCollection";
 import MyRecipe from "../../components/mypage/MyRecipe";
+// Style
+import { MyPageBox, OpenButton } from "../../styles/pages/MyPage.styled";
+// Image
+import { AiOutlineArrowUp } from "react-icons/ai";
 
 const MyPage = () => {
+  const [recipe, setRecipe] = useState(true);
+  const [favorite, setFavorite] = useState(true);
+
+  const recipeBtn = (e: React.MouseEvent<HTMLButtonElement>) =>
+    setRecipe(!recipe);
+
+  const favoriteBtn = (e: React.MouseEvent<HTMLButtonElement>) =>
+    setFavorite(!favorite);
+
   return (
-    <div>
-      <MyRecipe />
-      <FavoriteCollection />
-    </div>
+    <>
+      <OpenButton onClick={recipeBtn} fc={recipe}>
+        <div>
+          <AiOutlineArrowUp />
+        </div>
+        <p> 나만의 레시피 </p>
+        <div>
+          <AiOutlineArrowUp />
+        </div>
+      </OpenButton>
+
+      <MyPageBox open={recipe}>
+        <MyRecipe />
+      </MyPageBox>
+
+      <OpenButton onClick={favoriteBtn} fc={favorite}>
+        <div>
+          <AiOutlineArrowUp />
+        </div>
+        <p> 좋아하는 메뉴 </p>
+        <div>
+          <AiOutlineArrowUp />
+        </div>
+      </OpenButton>
+
+      <MyPageBox open={favorite}>
+        <FavoriteCollection />
+      </MyPageBox>
+    </>
   );
 };
 
